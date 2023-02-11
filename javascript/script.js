ee1578476786b4cc05a06222da377fe2ede4cbdf
iamBoluwatife <boluwatifeaiyenibe@gmail.com>
2023-02-11 20:30:14 +0100

    added script file

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+const loadingText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+const time = setInterval(display, 30);
+
+function display(){
+  load ++;
+  loadingText.innerText = `${load}%`;
+
+  if(load === 100){
+    clearInterval(time);
+  }
+
+  
+  loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max ) =>{
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+}
